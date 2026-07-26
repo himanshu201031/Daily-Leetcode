a579802ef8a9935d151915fca09bb0b806a47ee5
himanshu201031 <himanshu.bisai7075@gmail.com>
2026-07-26 20:18:08 +0530

    Refactor formatting and spacing in ProductofArrayExceptSelf class

--- Arrays/ProductofArrayExceptSelf.java
+++ Arrays/ProductofArrayExceptSelf.java
@@ -1,21 +1,27 @@
 public class ProductofArrayExceptSelf {
-    public static void main(String[] args) {
-        
-        int []num={1,2,3,5,3};
-        int n= num.length;
-        int ans[]=new int[n];
-ans[0]=1;
-        for(int i=1;i<n;i++){
-            ans[i]=ans[i-1]*num[i-1];
 
+    public static void main(String[] args) {
+
+        int[] num = {1, 2, 3, 5, 3};
+        int n = num.length;
+
+        int[] ans = new int[n];
+
+        ans[0] = 1;
+
+        for (int i = 1; i < n; i++) {
+            ans[i] = ans[i - 1] * num[i - 1];
         }
-    int suf=1;
-    for(int j= n-1;j>=0;j--){
-        ans[j]=ans[j]*suf;
-        suf=suf*num[j];
-    }
-;
-return;
+
+        int suf = 1;
+
+        for (int j = n - 1; j >= 0; j--) {
+            ans[j] = ans[j] * suf;
+            suf = suf * num[j];
+        }
+
+        for (int i = 0; i < n; i++) {
+            System.out.print(ans[i] + " ");
+        }
     }
 }
-
