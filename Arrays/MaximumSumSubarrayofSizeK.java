031ff56567f18ee06b98945e4282c1aa0c97dddf
himanshu201031 <himanshu.bisai7075@gmail.com>
2026-07-27 19:53:08 +0530

    Add Maximum Sum Subarray of Size K implementation

---
+++ Arrays/MaximumSumSubarrayofSizeK.java
@@ -0,0 +1,25 @@
+public class MaximumSumSubarrayofSizeK {
+    public static int getmax(int arr[], int k) {
+        int st = 0;
+        int sum = 0, maxsum = 0;
+        int n = arr.length;
+        for (int end = 0; end < n; end++) {
+            sum = sum + arr[end];
+            if (end >= k - 1) {
+                maxsum = Math.max(sum, maxsum);
+                sum = sum - arr[st];
+                st++;
+            }
+
+        }
+        return maxsum;
+    }
+
+    public static void main(String[] args) {
+        int[] arr = { 2, 3, 4, 5, 1, 6, 7 };
+        int k = 3;
+
+        int rt = getmax(arr, k);
+        System.out.println(rt);
+    }
+}
