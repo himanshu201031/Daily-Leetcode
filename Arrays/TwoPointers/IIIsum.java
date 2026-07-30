77ed43c5a2f5b004585825e74eb7025836fe8cde
himanshu201031 <himanshu.bisai7075@gmail.com>
2026-07-30 19:43:19 +0530

    day 22

---
+++ Arrays/TwoPointers/IIIsum.java
@@ -0,0 +1,62 @@
+package TwoPointers;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class IIIsum {
+    public  static List<List<Integer>> threeSum(int[] nums) {
+
+        List<List<Integer>> ans = new ArrayList<>();
+
+        Arrays.sort(nums);
+
+        int n = nums.length;
+
+        for (int i = 0; i < n - 2; i++) {
+
+           if(i>0 && nums[i]==nums[i-1])
+    continue;
+
+            int left = i + 1;
+            int right = n - 1;
+
+            while (left < right) {
+
+                int sum = nums[i] + nums[left] + nums[right];
+
+                if (sum == 0) {
+
+                    ans.add(Arrays.asList(nums[i], nums[left], nums[right]));
+
+                    left++;
+                    right--;
+                    while (left < right && nums[left] == nums[left - 1])
+                        left++;
+
+                    while (left < right && nums[right] == nums[right + 1])
+                        right--;
+                } else if (sum < 0) {
+
+                    left++;
+
+                } else {
+
+                    right--;
+
+                }
+
+            }
+
+        }
+
+        return ans;
+    }
+
+    public static void main(String[] args) {
+        int[] nums = { -1, 0, 1, 2, -1, -4 };
+        List<List<Integer>> rs = threeSum(nums);
+        System.out.print(rs);
+    }
+
+}
