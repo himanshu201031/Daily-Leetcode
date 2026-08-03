a223184aa5a6cd2e557d2f831a0fb72ec9cea74b
himanshu201031 <himanshu.bisai7075@gmail.com>
2026-08-03 16:47:59 +0530

    Day 26

---
+++ Arrays/Hashing/containDuplicates.java
@@ -0,0 +1,23 @@
+import java.util.HashSet;
+
+public class containDuplicates{
+    public boolean containsDuplicate(int[] nums) {
+HashSet<Integer> set = new HashSet<>();
+
+
+        for (int num : nums) {
+            if (set.contains(num)) {
+                return true;
+            }
+            set.add(num);
+        }
+        return false;
+    }
+
+    public static void main(String[] args) {
+        containDuplicates cd = new containDuplicates();
+        int[] nums = {1, 2, 3, 4, 5, 1};
+        boolean result = cd.containsDuplicate(nums);
+        System.out.println("Contains duplicates: " + result);
+    }
+}
