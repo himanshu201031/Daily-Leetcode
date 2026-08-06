f8499547587c7da60a1076b324c092085fe3d501
himanshu201031 <himanshu.bisai7075@gmail.com>
2026-08-06 19:52:07 +0530

    Day 28

--- Arrays/TwoPointers/twosumII.java
+++ Arrays/TwoPointers/twosumII.java
@@ -8,7 +8,7 @@ public class twosumII {
         while (lt < rt) {
             sum = arr[lt] + arr[rt];
             if (sum == target) {
-                return new int[] { lt , rt  };
+                return new int[] { lt+1 , rt+1 };
             } else if (sum > target) {
                 rt--;
             } else {
