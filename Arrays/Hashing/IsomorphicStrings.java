72347ea6f40372b0f6c78d5400b5f0570951f2ab
himanshu201031 <himanshu.bisai7075@gmail.com>
2026-08-07 17:51:17 +0530

    Day 29

---
+++ Arrays/Hashing/IsomorphicStrings.java
@@ -0,0 +1,36 @@
+package Hashing;
+
+public class IsomorphicStrings {
+    public boolean isIsomorphic(String s, String t) {
+        if (s.length() != t.length()) {
+            return false;
+        }
+
+        int[] mapS = new int[256];
+        int[] mapT = new int[256];
+
+        for (int i = 0; i < s.length(); i++) {
+            char charS = s.charAt(i);
+            char charT = t.charAt(i);
+
+            if (mapS[charS] != mapT[charT]) {
+                return false;
+            }
+
+            mapS[charS] = i + 1;
+            mapT[charT] = i + 1;
+        }
+
+        return true;
+    }
+    public static void main(String[] args) {
+        IsomorphicStrings is = new IsomorphicStrings();
+
+        String s = "egg";
+        String t = "add";
+
+        boolean result = is.isIsomorphic(s, t);
+
+        System.out.println("Is isomorphic: " + result);
+    }
+}
